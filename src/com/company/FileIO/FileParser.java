b3d287e2efc5e23df1c8a9f90666f81aac540912
mjh9131 <mjh9131@rit.edu>
2020-02-23 01:32:04 -0500

    commented the file parser

--- src/com/company/FileIO/FileParser.java
+++ src/com/company/FileIO/FileParser.java
@@ -5,33 +5,74 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * @author mjh9131
+ *
+ * given a file path and a file name, this class will store them
+ * the user can also retrieve and change the file name and file path in question
+ * when the user wants, they can try to parse the file
+ * if the file is a csv file it will parse it and return an array list of string arrays
+ * each string array in the arraylist being the line in the file
+ * and the entries in the string array being the comma seperated fields
+ *
+ */
 public class FileParser {
 
+    /**
+     * Attributes
+     */
     private String filePath;
     private String fileName;
 
+    /**
+     * Constructor
+     * @param filePath
+     * @param fileName
+     */
     public FileParser(String filePath, String fileName) {
         this.filePath = filePath;
         this.fileName = fileName;
-
     }
 
+    /**
+     * getter for the filepath
+     * @return filePath
+     */
     public String getFilePath() {
         return filePath;
     }
 
+    /**
+     * setter for filePath
+     * @param filePath
+     */
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     *getter for the fileName
+     * @return fileName
+     */
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * setter for the fileName
+     * @param fileName
+     */
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
+    /**
+     * Uses the saved filepath and filename to read through the file
+     * for each line. split it into a string array based on the commas,
+     * then add it to an ArrayList
+     *
+     * @return Arraylist of all the lines, which are String arrays containg the csvs
+     */
     public ArrayList<String[]> readFile(){
         try{
             var path = Paths.get(filePath, fileName);
