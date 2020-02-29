8dc5f740edeefc9df62d86d0bb99ef1d6ef6f5f4
mjh9131 <mjh9131@rit.edu>
2020-02-29 15:07:47 -0500

    started working on the file saver

--- src/com/company/FileIO/FileSaver.java
+++ src/com/company/FileIO/FileSaver.java
@@ -1,4 +1,14 @@
 package com.company.FileIO;
 
 public class FileSaver {
+
+    private static FileSaver fileSaver = null;
+
+    public static FileSaver getInstance(){
+        if(fileSaver == null){
+            fileSaver = new FileSaver();
+        }
+
+        return fileSaver;
+    }
 }
