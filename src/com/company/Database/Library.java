df59dfbeda642edf029d574dfe140593165b0158
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2020-02-29 01:50:36 -0500

    Created Library

--- src/com/company/Database/Library.java
+++ src/com/company/Database/Library.java
@@ -1,5 +1,34 @@
 package com.company.Database;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Library
+ * 
+ */
 public class Library {
+
+    private String username = "Jimmy";
+    private HashSet<Searchable> searchable;
+    // TODO: Decide to keep using HashMap or switch to TreeSet
+    //              GUID  , rating
+    private HashMap<String, Integer> ratings;
+
+    public HashSet<Searchable> getSearchable() {
+        return searchable;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean addSearchable(String searchableGUID) {
+
+        return false;
+
+    }
+
+
 
 }
